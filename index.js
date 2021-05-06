2719f538ae520854f2dddd769a402b44b4114b43
gavin-rilee <you@example.com>
2021-05-06 13:56:02 -0400

    pushing a frontend

--- index.js
+++ index.js
@@ -1,9 +1,11 @@
 require('dotenv').config()
-
+const path = require('path')
 console.log('Is this thing on?')
 const express = require('express')
 
 const PORT = process.env.PORT || 5000
+
+server.use(express.static(path.join(__dirname, 'client/build')))
 
 const server = express();
 
@@ -15,6 +17,9 @@ server.get('/api/*', (req, res) =>{
     })
 })
 
+server.use('*', (req, res) =>{
+    res.sendFile(path.join(__dirname, 'client/build/', 'index.html'))
+})
 
 server.listen(PORT, () => {
     console.log(`listening on ${PORT}`)
